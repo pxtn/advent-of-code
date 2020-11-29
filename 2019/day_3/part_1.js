c10db439f24e76da56671d7dbfef0ddfb0283eb1
Andreas Willi <andiwilli8460@gmail.com>
2020-11-29 19:49:26 +0100

    Add 2019/day_3

---
+++ 2019/day_3/part_1.js
@@ -0,0 +1,137 @@
+// Day 3: Crossed Wires
+// Part One
+let { input } = require("./input.js");
+
+const wire1 = input[0];
+const wire2 = input[1];
+
+// 1 = wire1 runs through this point
+// (2 = wire2 runs through this point)
+// 3 = both wires run through this point
+const grid = new Map();
+
+let xPos = 0;
+let yPos = 0;
+wire1.forEach((path) => {
+  const direction = path[0];
+  const distance = Number(path.substr(1));
+
+  switch (direction) {
+    case "U":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos - i)) {
+          grid.set(yPos - i, new Map());
+        }
+        const row = grid.get(yPos - i);
+        row.set(xPos, 1);
+      }
+      yPos -= distance;
+      break;
+    case "D":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos + i)) {
+          grid.set(yPos + i, new Map());
+        }
+        const row = grid.get(yPos + i);
+        row.set(xPos, 1);
+      }
+      yPos += distance;
+      break;
+    case "L":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos)) {
+          grid.set(yPos, new Map());
+        }
+        const row = grid.get(yPos);
+        row.set(xPos - i, 1);
+      }
+      xPos -= distance;
+      break;
+    case "R":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos)) {
+          grid.set(yPos, new Map());
+        }
+        const row = grid.get(yPos);
+        row.set(xPos + i, 1);
+      }
+      xPos += distance;
+      break;
+    default:
+      console.error("unknown direction", direction);
+      return;
+  }
+});
+
+xPos = 0;
+yPos = 0;
+wire2.forEach((path) => {
+  const direction = path[0];
+  const distance = Number(path.substr(1));
+
+  switch (direction) {
+    case "U":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos - i)) {
+          grid.set(yPos - i, new Map());
+        }
+        const row = grid.get(yPos - i);
+        if (row.get(xPos) === 1) {
+          row.set(xPos, 3);
+        }
+      }
+      yPos -= distance;
+      break;
+    case "D":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos + i)) {
+          grid.set(yPos + i, new Map());
+        }
+        const row = grid.get(yPos + i);
+        if (row.get(xPos) === 1) {
+          row.set(xPos, 3);
+        }
+      }
+      yPos += distance;
+      break;
+    case "L":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos)) {
+          grid.set(yPos, new Map());
+        }
+        const row = grid.get(yPos);
+        if (row.get(xPos - i) === 1) {
+          row.set(xPos - i, 3);
+        }
+      }
+      xPos -= distance;
+      break;
+    case "R":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos)) {
+          grid.set(yPos, new Map());
+        }
+        const row = grid.get(yPos);
+        if (row.get(xPos + i) === 1) {
+          row.set(xPos + i, 3);
+        }
+      }
+      xPos += distance;
+      break;
+    default:
+      console.error("unknown direction", direction);
+      return;
+  }
+});
+
+let output = Infinity;
+
+grid.forEach((row, y) => {
+  row.forEach((value, x) => {
+    if (value === 3) {
+      output = Math.min(output, Math.abs(y) + Math.abs(x));
+    }
+  });
+});
+
+console.log(output); // 1285
---
+++ 2019/day_3/part_2.js
@@ -0,0 +1,142 @@
+// Day 3: Crossed Wires
+// Part Two
+let { input } = require("./input.js");
+
+const wire1 = input[0];
+const wire2 = input[1];
+
+const grid = new Map();
+
+let totalDistance = 0;
+let xPos = 0;
+let yPos = 0;
+wire1.forEach((path) => {
+  const direction = path[0];
+  const distance = Number(path.substr(1));
+
+  switch (direction) {
+    case "U":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos - i)) {
+          grid.set(yPos - i, new Map());
+        }
+        const row = grid.get(yPos - i);
+        row.set(xPos, { wire1: totalDistance + i });
+      }
+      yPos -= distance;
+      break;
+    case "D":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos + i)) {
+          grid.set(yPos + i, new Map());
+        }
+        const row = grid.get(yPos + i);
+        row.set(xPos, { wire1: totalDistance + i });
+      }
+      yPos += distance;
+      break;
+    case "L":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos)) {
+          grid.set(yPos, new Map());
+        }
+        const row = grid.get(yPos);
+        row.set(xPos - i, { wire1: totalDistance + i });
+      }
+      xPos -= distance;
+      break;
+    case "R":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos)) {
+          grid.set(yPos, new Map());
+        }
+        const row = grid.get(yPos);
+        row.set(xPos + i, { wire1: totalDistance + i });
+      }
+      xPos += distance;
+      break;
+    default:
+      console.error("unknown direction", direction);
+      return;
+  }
+  totalDistance += distance;
+});
+
+totalDistance = 0;
+xPos = 0;
+yPos = 0;
+wire2.forEach((path) => {
+  const direction = path[0];
+  const distance = Number(path.substr(1));
+
+  switch (direction) {
+    case "U":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos - i)) {
+          grid.set(yPos - i, new Map());
+        }
+        const row = grid.get(yPos - i);
+        const value = row.get(xPos);
+        if (!value || value.wire2 == undefined) {
+          row.set(xPos, { ...value, wire2: totalDistance + i });
+        }
+      }
+      yPos -= distance;
+      break;
+    case "D":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos + i)) {
+          grid.set(yPos + i, new Map());
+        }
+        const row = grid.get(yPos + i);
+        const value = row.get(xPos);
+        if (!value || value.wire2 == undefined) {
+          row.set(xPos, { ...value, wire2: totalDistance + i });
+        }
+      }
+      yPos += distance;
+      break;
+    case "L":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos)) {
+          grid.set(yPos, new Map());
+        }
+        const row = grid.get(yPos);
+        const value = row.get(xPos - i);
+        if (!value || value.wire2 == undefined) {
+          row.set(xPos - i, { ...value, wire2: totalDistance + i });
+        }
+      }
+      xPos -= distance;
+      break;
+    case "R":
+      for (let i = 1; i <= distance; i++) {
+        if (!grid.get(yPos)) {
+          grid.set(yPos, new Map());
+        }
+        const row = grid.get(yPos);
+        const value = row.get(xPos + i);
+        if (!value || value.wire2 == undefined) {
+          row.set(xPos + i, { ...value, wire2: totalDistance + i });
+        }
+      }
+      xPos += distance;
+      break;
+    default:
+      console.error("unknown direction", direction);
+      return;
+  }
+  totalDistance += distance;
+});
+
+let output = Infinity;
+
+grid.forEach((row) => {
+  row.forEach((value) => {
+    if (value && value.wire1 != undefined && value.wire2 != undefined) {
+      output = Math.min(output, value.wire1 + value.wire2);
+    }
+  });
+});
+
+console.log(output); // 14228
