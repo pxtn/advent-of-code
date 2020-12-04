7c3146ba4475a79107f3630d640692a00ac19ed1
Pascal Affentranger <affentranger.pascal@gmail.com>
2020-12-04 15:28:21 +0100

    day 04 part 1

---
+++ 2020/day_04/part_1.js
@@ -0,0 +1,41 @@
+// Day 4: Passport Processing
+// Part One
+const { input } = require("./input.js");
+const { run } = require("../../util/run.js");
+
+// should ignore cid field
+const requiredFields = [
+  'byr',
+  'iyr',
+  'eyr',
+  'hgt',
+  'hcl',
+  'ecl',
+  'pid',
+];
+
+const func = () => {
+
+  let output = 0;
+
+  input.split('\n\n').forEach(entry => {
+    console.log(entry + '\n');
+    if (checkIfFieldsAreAvailable(entry)) {
+      output++;
+    }
+  });
+
+  return {output};
+}
+
+const checkIfFieldsAreAvailable = (entry) => {
+  let isValid = true;
+  requiredFields.forEach(field => {
+    if (!entry.includes(field)) {
+      isValid = false;
+    }
+  })
+  return isValid;
+}
+
+run(func);
