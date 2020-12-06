c1eb1d9e68b35d5963a4560bb6026e4f88349a4a
Pascal Affentranger <affentranger.pascal@gmail.com>
2020-12-07 00:58:16 +0100

    day 05: cleaned up part 01

--- 2020/day_05/part_1.js
+++ 2020/day_05/part_1.js
@@ -11,12 +11,13 @@ const func = () => {
     input.split('\n').forEach(seatSequence => {
         let binaryRow = seatSequence.substr(0, 7).replace(/B/g,1).replace(/F/g,0);
         let binaryColumn = seatSequence.substr(7, 3).replace(/R/g,1).replace(/L/g,0);
-        seatArray.push({initialSequence: seatSequence, binarySequence: binaryRow + binaryColumn, binaryRow: binaryRow, binaryColumn: binaryColumn});
+        let id = parseInt(binaryRow, 2) * 8 + parseInt(binaryColumn, 2);
+
+        seatArray.push({id: id});
     })
 
-    seatArray.sort((a, b) => a.binarySequence - b.binarySequence);
-    const seatWithHighestId = seatArray.pop();
-    output = parseInt(seatWithHighestId.binaryRow, 2) * 8 + parseInt(seatWithHighestId.binaryColumn, 2);
+    seatArray.sort((a, b) => a.id - b.id);
+    output = seatArray.pop().id;
 
     return {output};
 }
